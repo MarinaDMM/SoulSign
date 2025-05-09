1cc84c3b143f7c5b31c221989e498050508ab578
MarinaDMM <dedikova.mm@gmail.com>
2025-05-09 13:27:52 +0200

    Initial Commit

---
+++ SoulSign/SoulSignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoulSignApp.swift
+//  SoulSign
+//
+//  Created by Marina Dedikova on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SoulSignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
